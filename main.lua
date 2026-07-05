e6efa46a4a77837907644c8e10d03c1bffe4379d
TunaCANNN <jaydenhanner88@gmail.com>
2026-07-05 18:35:08 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,13 @@
+--// DeepSearch v12 - Main Loader
+print("[DeepSearch v12] Loading...")
 
+-- Manually choose version: "full" or "lite"
+local version = "full"
+
+if version == "full" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TunaCANNN/DeepSearch/main/versions/full.lua"))()
+elseif version == "lite" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TunaCANNN/DeepSearch/main/versions/lite.lua"))()
+else
+    warn("[DeepSearch] Invalid version selected. Use 'full' or 'lite'")
+end
